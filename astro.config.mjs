ae75d79d8326f616b67ebc3cdec8a56062d9101e
Jack Woodrow <jackwoodrow99@gmail.com>
2023-04-15 15:53:51 -0400

    Add github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://jwoodrow99.github.io',
+  base: '/homepage',
 });
